2333df949ada337ec838c63c1030ef1a9764ce81
elftausend <76885970+elftausend@users.noreply.github.com>
2024-01-07 17:57:51 +0100

    Add channels struct, entities file

---
+++ src/entities.rs
@@ -0,0 +1,32 @@
+use tokio::{net::TcpStream, io::AsyncWriteExt};
+
+use crate::PositionAndLook;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Type {
+    Player
+}
+
+pub async fn spawned_named_entity(
+    stream: &mut TcpStream,
+    eid: i32,
+    name: &str,
+    pos_and_look: &PositionAndLook,
+) {
+    let mut named_entity_spawn = vec![0x14];
+    named_entity_spawn.extend_from_slice(&eid.to_be_bytes());
+
+    named_entity_spawn.extend_from_slice(&(name.len() as i16).to_be_bytes());
+    named_entity_spawn.extend_from_slice(name.as_bytes());
+    let x = (pos_and_look.x * 32.).round() as i32;
+    let y = (pos_and_look.y * 32.).round() as i32;
+    let z = (pos_and_look.z * 32.).round() as i32;
+
+    named_entity_spawn.extend_from_slice(&x.to_be_bytes());
+    named_entity_spawn.extend_from_slice(&y.to_be_bytes());
+    named_entity_spawn.extend_from_slice(&z.to_be_bytes());
+    named_entity_spawn.extend_from_slice(&[0, 0, 0, 0]);
+
+    stream.write_all(&named_entity_spawn).await.unwrap();
+    stream.flush().await.unwrap();
+}
